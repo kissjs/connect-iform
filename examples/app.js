2df74bd1fa5a3926796d24bb9039a9cbb4a6c51a
Lin Gui <guileen@gmail.com>
2011-11-19 21:59:52 +0800

    add examples

---
+++ examples/app.js
@@ -0,0 +1,51 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , iform = require('../iform')
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+var userForm = iform({
+    username: {
+      len: [3, 10]
+    }
+  , email: 'email'
+  , password: {
+      required: true
+    , len: 3
+    }
+  , age : 'int'
+  , floatField : 'float'
+  , birth: Date
+});
+
+// Routes
+app.get('/', function(req, res, next){
+    res.render('index', {
+        title : 'Hello'
+    });
+});
+
+app.post('/', userForm(), function(req, res, next) {
+    if(req.iform.errors) {
+      return res.json(req.iform.errors);
+    }
+    res.json(req.iform.data);
+});
+
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
